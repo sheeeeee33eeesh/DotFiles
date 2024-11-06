808cf0bb3046fad71db1f7fc5ed15f84f866567d
sheeeeee33eeesh <tylber5338@students.ecpi.edu>
2024-11-06 15:33:44 -0500

    Updated nonroot-setupconsole.sh

--- scripts/notroot-setupconsole.sh
+++ scripts/notroot-setupconsole.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-mkdir -p ~/.config/fish/ ~/Desktop
+mkdir -p ~/.config/ ~/Desktop
 mv ./fish/ ~/.config/
-fish -c "curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher $(cat ~/.config/fish/fish_plugins | tr '\n' ' ')"
+fish -c "curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && cat ~/.config/fish/fish_plugins | while read line ; fisher install $line ; end"
 
 git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
