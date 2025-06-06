3f02b0d3906bc60e4ca813992c249c6959f284bf
sheeeeee33eeesh <tylber5338@students.ecpi.edu>
2025-06-06 16:46:54 -0400

    Updated Settings

--- hypr/scripts/Volume.sh
+++ hypr/scripts/Volume.sh
@@ -1,22 +1,22 @@
 #!/usr/bin/env bash
 if [ "$1" == "up5" ]; then
-   for i in {0..5}; do
+   for i in {0..8}; do
       pactl set-sink-volume $i +5% &
    done
 elif [ "$1" == "down5" ]; then
-   for i in {0..5}; do
+   for i in {0..8}; do
       pactl set-sink-volume $i -5% &
    done
 elif [ "$1" == "up1" ]; then
-   for i in {0..5}; do
+   for i in {0..8}; do
       pactl set-sink-volume $i +1% &
    done
 elif [ "$1" == "down1" ]; then
-   for i in {0..5}; do
+   for i in {0..8}; do
       pactl set-sink-volume $i -1% &
    done
 elif [ "$1" == "mute" ]; then
-   for i in {0..5}; do
+   for i in {0..8}; do
       pactl set-sink-mute $i toggle &
    done
 else
--- nvim/lua/plugins/none-ls.lua
+++ nvim/lua/plugins/none-ls.lua
@@ -1,25 +1,25 @@
 return {
-	{
-		"nvimtools/none-ls.nvim",
+  {
+    "nvimtools/none-ls.nvim",
     lazy = true,
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-		},
-		config = function()
-			local null_ls = require("null-ls")
-			null_ls.setup({
-				sources = {
-					null_ls.builtins.formatting.stylua,
-					null_ls.builtins.formatting.clang_format,
-					null_ls.builtins.formatting.shfmt,
-					null_ls.builtins.formatting.black,
-					null_ls.builtins.formatting.pyink,
-					null_ls.builtins.formatting.shellharden,
-					null_ls.builtins.diagnostics.selene,
-					null_ls.builtins.diagnostics.mypy,
-				},
-			})
-			vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-		end,
-	},
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    config = function()
+      local null_ls = require("null-ls")
+      null_ls.setup({
+        sources = {
+          null_ls.builtins.formatting.stylua,
+          null_ls.builtins.formatting.clang_format,
+          null_ls.builtins.formatting.shfmt,
+          null_ls.builtins.formatting.black,
+          null_ls.builtins.formatting.pyink,
+          null_ls.builtins.formatting.shellharden,
+          null_ls.builtins.diagnostics.selene,
+          null_ls.builtins.diagnostics.mypy,
+        },
+      })
+      vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+    end,
+  },
 }
