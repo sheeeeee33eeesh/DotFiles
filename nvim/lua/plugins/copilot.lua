38d5d329c0eedbc990837408985134c7ab68b02f
sheeeee33eeesh <tylber5338@students.ecpi.edu>
2025-06-14 17:52:34 -0400

    Commented Out Copilot

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -1,38 +1,40 @@
 return {
-	{
-		"zbirenbaum/copilot.lua",
-    lazy = false,
-		opts = {
-			panel = {
-				enabled = true,
-				auto_refresh = true,
-				keymap = {
-					jump_prev = "[[",
-					jump_next = "]]",
-					accept = "<CR>",
-					refresh = "gr",
-					open = "<M-CR>",
-				},
-				layout = {
-					position = "right",
-					ratio = 0.4,
-				},
-			},
-			suggestion = {
-				enabled = true,
-				auto_trigger = true,
-				hide_during_completion = true,
-				debounce = 75,
-				trigger_on_accept = true,
-				keymap = {
-					accept = "<C-a>",
-					accept_word = false,
-					accept_line = false,
-					next = "<M-]>",
-					prev = "<M-[>",
-					dismiss = "<C-]>",
-				},
-			},
-		},
-	},
+  ---- Uncomment The Following Lines To Enable Copilot
+	-- {
+	-- 	"zbirenbaum/copilot.lua",
+  --   enable = false,
+  --   lazy = false,
+	-- 	opts = {
+	-- 		panel = {
+	-- 			enabled = true,
+	-- 			auto_refresh = true,
+	-- 			keymap = {
+	-- 				jump_prev = "[[",
+	-- 				jump_next = "]]",
+	-- 				accept = "<CR>",
+	-- 				refresh = "gr",
+	-- 				open = "<M-CR>",
+	-- 			},
+	-- 			layout = {
+	-- 				position = "right",
+	-- 				ratio = 0.4,
+	-- 			},
+	-- 		},
+	-- 		suggestion = {
+	-- 			enabled = true,
+	-- 			auto_trigger = true,
+	-- 			hide_during_completion = true,
+	-- 			debounce = 75,
+	-- 			trigger_on_accept = true,
+	-- 			keymap = {
+	-- 				accept = "<C-a>",
+	-- 				accept_word = false,
+	-- 				accept_line = false,
+	-- 				next = "<M-]>",
+	-- 				prev = "<M-[>",
+	-- 				dismiss = "<C-]>",
+	-- 			},
+	-- 		},
+	-- 	},
+	-- },
 }
